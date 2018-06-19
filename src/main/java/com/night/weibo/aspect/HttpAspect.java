b187677e6e309d65530704037ea36e293038f664
Liao Fushen <liaofushen@outlook.com>
2018-06-19 12:07:52 +0800

    jsp 视图配置

---
+++ src/main/java/com/night/weibo/aspect/HttpAspect.java
@@ -0,0 +1,11 @@
+package com.night.weibo.aspect;
+
+import lombok.extern.java.Log;
+import org.aspectj.lang.annotation.Aspect;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+@Log
+public class HttpAspect {
+}
